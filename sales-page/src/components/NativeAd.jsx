0c045f3f8f79e6c35f806081952fb27faf89d8c9
bobprince <bobprince87@gmail.com>
2025-11-24 16:07:07 -0500

    Added NativeAd component in place of AdsterraAd in Hero and Footer components.

--- sales-page/src/components/NativeAd.jsx
+++ sales-page/src/components/NativeAd.jsx
@@ -2,16 +2,22 @@ import { useEffect } from "react";
 
 const NativeAd = () => {
   useEffect(() => {
+    // Clear old content (in case of re-render)
+    const container = document.getElementById(
+      "container-bc4e72841afc30cbf846f4a4a06a4901"
+    );
+    if (!container) return;
+
+    container.innerHTML = ""; // Clear previous script if any
+
+    // Create script element
     const script = document.createElement("script");
     script.async = true;
     script.setAttribute("data-cfasync", "false");
     script.src =
       "//pl28126322.effectivegatecpm.com/bc4e72841afc30cbf846f4a4a06a4901/invoke.js";
 
-    const container = document.getElementById(
-      "container-bc4e72841afc30cbf846f4a4a06a4901"
-    );
-    if (container) container.appendChild(script);
+    container.appendChild(script);
   }, []);
 
   return (
